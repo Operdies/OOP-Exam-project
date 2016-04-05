f6c6246c0fc821221976a75f425e0722982efe56
Alexander Larsen <alexnlarsen@gmail.com>
2016-04-05 12:46:19 +0200

    Template for OOP Exam

---
+++ Eksamensopgave2016/Program.cs
@@ -0,0 +1,20 @@
+﻿//20154304_Alexander_Nørøskov_Larsen
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eksamensopgave2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IStregsystem stregsystem = new Stregsystem();
+            IStregsystemUI ui = new StregsystemCLI(stregsystem);
+            StregsystemController sc = new StregsystemController(ui, stregsystem);
+
+            ui.Start();
+        }
+    }
+}
